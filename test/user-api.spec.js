2e3e020a883951d93341e97a6404802612166691
Emil <emil.kais94@gmail.com>
2017-10-29 23:50:51 -0400

    added a test for api request on sending a url and getting a list of information from rating to votes to imdb link to the trailer

--- test/user-api.spec.js
+++ test/user-api.spec.js
@@ -2,7 +2,7 @@ var request = require('request');
 var assert = require('assert');
 var server = require('../index.js');
 
-var base_url = "httpe://localhost:3000/";
+var base_url = "http://localhost:3000/";
 
 describe("query page", function() {
 
@@ -14,6 +14,26 @@ describe("query page", function() {
         done();
       });
     });
+  });
+
+  describe("get /api/movie for sing", function() {
+    let movieUrl = "https://content.viaplay.se/pc-se/film/sing-2016";
+    let movieTitle = "sing-2016";
+    let movieImdb = "tt3470600";
+    let movieRating = "7.1";
+    let movieTrailerUrl = "http://www.imdb.com/title/tt3470600?ref_ext_viaplay";
+    let movieYouTubeLink = "https://www.youtube.com/watch?v=9zXiaDhAEwY"
+    it("first movieUrl request", function() {
+      request.post(base_url + '/api/movie', function(err, res) {
+        assert.equal(movieTitle, res.title);
+        assert.equal(movieImdb, res.id);
+        assert.equal(movieRating, res.rating);
+        assert.equal(movieTrailerUrl, res.url);
+        server.closeServer();
+        done();
+      });
+    });
+
   });
 
 });
