bab5241aeefbbd86332cafe38a5db4db38e14d98
Emil <emil.kais94@gmail.com>
2017-11-06 13:52:14 -0500

    started groundwork for utilizing winston and nconf as well as using nginx in the future

--- index.js
+++ index.js
@@ -2,13 +2,38 @@
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
 var config = require('./app/config/config');
 var bodyParser = require('body-parser');
 var compression = require('compression');
+var morgan = require('morgan');
 var helmet = require('helmet');
+var nconf = require('nconf');
+var winston = require('winston');
+
+winston.add(winston.transports.File, {"filename": "error.log", "level": nconf.get("logger")});
+
 var routes = require('./app/routes');
 var server = require('http').Server(app);
+
+nconf.argv({
+  'p': {
+    'alias': 'http:port',
+    'describe': 'The port to listen on'
+  }
+});
+
+nconf.file("./app/config/config.json")
+nconf.defaults({
+  "http": {
+    "port": 3000
+  },
+  "logger": {
+    "fileLevel": "error"
+  }
+});
+
+winston.info('Initialized nconf');
+winston.info('HTTP Config: ', nconf.get("http"));
 
 //used to gzip contents to send more info to client
 app.use(compression());
@@ -20,11 +45,12 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 app.use(express.static('public'));
 
 app.use('/', routes);
 
-server.listen(port);
+server.listen(nconf.get('http:port'));
 
 exports.closeServer = function() {
   server.close();
